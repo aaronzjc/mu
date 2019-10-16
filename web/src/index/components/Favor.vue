77ec4b18fa37cc450b7cf823b25be6e963e38283
JinchengZhang <jincheng3@staff.weibo.com>
2019-10-17 00:40:45 +0800

    fix icon

--- web/src/index/components/Favor.vue
+++ web/src/index/components/Favor.vue
@@ -27,7 +27,7 @@
                     </div>
                     <div class="divider"></div>
                     <div class="hot-opt" @click="remove(idx)">
-                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <svg style="width:20px;height:20px" viewBox="0 0 24 24">
                             <path fill="#ff7474" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
                         </svg>
                     </div>
