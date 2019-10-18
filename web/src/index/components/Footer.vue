95be32453a3e1cb4fb238f0c177a4eb10a5b6ee3
JinchengZhang <jincheng3@staff.weibo.com>
2019-10-18 19:26:21 +0800

    fix doc

--- web/src/index/components/Footer.vue
+++ web/src/index/components/Footer.vue
@@ -2,7 +2,7 @@
 <div class="columns">
     <div class="column copyright has-text-centered">
         <p class="user-declare has-text-dark">站点仅供学习交流使用，如有侵权，请联系下线</p>
-        <p><a href="https://github.com/aaronzjc">@aaronzjc</a>开发, 源码<a href="https://github.com/aaronzjc/crawler">在此</a>, 欢迎Star.</p>
+        <p><a href="https://github.com/aaronzjc">@aaronzjc</a>开发, 源码<a href="https://github.com/aaronzjc/mu">在此</a>, 欢迎Star.</p>
         <p class="backtop"><a href="javascript:scrollTo(0,0);">回到顶部</a></p>
     </div>
 </div>
