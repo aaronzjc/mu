8fb80299fe66bae5e94dea9c196482759d25cb71
jincheng <jincheng3@staff.weibo.com>
2020-04-18 01:29:17 +0800

    ua

--- internal/svc/lib/def.go
+++ internal/svc/lib/def.go
@@ -88,7 +88,7 @@ func CrawHTML(link Link, headers map[string]string) (Page, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", link.Url, nil)
 	if _, ok := headers["User-Agent"]; !ok {
-		req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36`)
+		req.Header.Add("User-Agent", `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36`)
 	}
 	if len(headers) > 0 {
 		for k, v := range headers {
