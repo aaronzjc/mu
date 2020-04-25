d911f0cf928759d6332e24f912c3745469b239f6
jincheng <jincheng3@staff.weibo.com>
2020-04-25 19:39:39 +0800

    imp pack

--- web/vue.config.js
+++ web/vue.config.js
@@ -7,20 +7,17 @@ module.exports = {
             template: "public/index.html",
             filename: "index.html",
             title: "首页",
-            chunks: ['chunk-vendors', 'chunk-common', 'chunk-index-vendors', 'index']
         },
         admin: {
             entry: "src/admin/main.js",
             template: "public/admin.html",
             filename: "admin.html",
             title: "后台",
-            chunks: ['chunk-vendors', 'chunk-common', 'chunk-admin-vendors', 'admin']
         }
     },
-    configureWebpack:{
-        performance: {
-            hints: false
-        }
+    chainWebpack: config => {
+        config.performance.hints = false
+        config.optimization.delete('splitChunks')
     },
     devServer: {
         disableHostCheck: true
