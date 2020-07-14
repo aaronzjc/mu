e508efdb29ee980f23da90c12a65844b58e1f858
jincheng <415397228@qq.com>
2020-07-14 22:53:32 +0800

    multiple commander

--- cmd/commander/main.go
+++ cmd/commander/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"mu/internal/app/commander"
-	"mu/internal/svc/schedule"
 )
 
-func init() {
-	// 注册定时任务
-	schedule.JobSchedule.InitJobs()
-}
-
 func main() {
+	// 初始化
+	commander.InitCommander()
+
 	addr := ":7970"
 	commander.RegisterRpcServer(addr)
 }
--- internal/app/commander/commander.go
+++ internal/app/commander/commander.go
@@ -5,15 +5,26 @@ import (
 	"google.golang.org/grpc"
 	"mu/internal/svc/rpc"
 	"mu/internal/svc/schedule"
+	"mu/internal/util/cache"
 	"mu/internal/util/logger"
 	"net"
+	"time"
+)
+
+const idMachine = "is_machine"
+const jobVisor = "job_visor"
+const election = "election"
+
+var (
+	id	int
+	isLeader bool
 )
 
 type CommanderServer struct{}
 
 func (commander *CommanderServer) UpdateCron(ctx context.Context, req *rpc.Cron) (*rpc.CronRes, error) {
-	schedule.JobSchedule.UpdateJob(req.Site)
-	logger.Info("Rpc UpdateCron [site = %s] success !", req.Site)
+	redis := cache.RedisConn()
+	redis.LPush(jobVisor, req.Site)
 	return &rpc.CronRes{Success: true}, nil
 }
 
@@ -28,4 +39,49 @@ func RegisterRpcServer(addr string) {
 	rpc.RegisterCommanderServer(rpcServer, &CommanderServer{})
 	logger.Info("Commanderserver is listening on :7970")
 	logger.Fatal(rpcServer.Serve(lis))
+}
+
+// 初始化，分配一个ID，并且选择一个主节点
+func InitCommander() bool {
+	// 选择一个主节点
+	redis := cache.RedisConn()
+
+	// 获取当前节点ID
+	idRes := redis.Incr(idMachine)
+	id = int(idRes.Val())
+
+	// 选择一个节点作为leader
+	if ok := redis.SetNX(election, id, time.Minute); ok.Val() {
+		isLeader = true
+	}
+
+	// 注册定时任务
+	if isLeader {
+		schedule.JobSchedule.InitJobs()
+	}
+
+	return isLeader
+}
+
+// 监听队列里面任务
+func JobVisor() {
+	if !isLeader {
+		return
+	}
+
+	redis := cache.RedisConn()
+	for {
+		data := redis.LPop(jobVisor)
+		if data.Val() == "" {
+			time.Sleep(time.Second * 5)
+			continue
+		}
+		schedule.JobSchedule.UpdateJob(data.Val())
+		logger.Info("Rpc UpdateCron [site = %s] success !", data)
+	}
+}
+
+// 管理master的健康状态
+func MasterVisor() {
+	// TODO: 管理
 }
