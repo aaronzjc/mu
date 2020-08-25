3e635ed4ad013aa658605cf505799ec6c130fb5a
jincheng <jincheng3@staff.weibo.com>
2020-08-25 19:18:18 +0800

    remove pprof

--- cmd/commander/main.go
+++ cmd/commander/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"log"
 	"mu/internal/app/commander"
-	"net/http"
-	_ "net/http/pprof" // 必须，引入 pprof 模块
 )
 
 func main() {
 	// 初始化
 	commander.InitCommander()
-
-	go func() {
-		log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
-	}()
 
 	addr := ":7970"
 	commander.RegisterRpcServer(addr)
