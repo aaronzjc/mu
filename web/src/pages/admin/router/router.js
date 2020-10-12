18f8eb7a62c7b4ff1977ec135962c30ff2e96d8d
jincheng <jincheng3@staff.weibo.com>
2020-10-12 10:38:41 +0800

    fix route

--- web/src/pages/admin/router/router.js
+++ web/src/pages/admin/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Site from "../components/Site"
 import Node from "../components/Node"
--- web/src/pages/index/router/router.js
+++ web/src/pages/index/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Main from "../components/Main";
 import Index from "../components/Index";
 
